e448c7eb6f47d184b34485a9557074d0e9a79a01
cphackm <cphackm@gmail.com>
2014-06-29 14:55:02 -0700

    Initial commit of AbstractSpriteObject

---
+++ BananaBoat/GameObjects/GraphicalObjects/AbstractSpriteObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace BananaBoat.GameObjects.GraphicalObjects
+{
+	public abstract class AbstractSpriteObject : AbstractGameObject
+	{
+		protected float x { set; get; }
+		protected float y { set; get; }
+		protected float angle { set; get; }
+		protected float scale { set; get; }
+		protected Color color { set; get; }
+
+		public abstract void Update();
+		public abstract void Render();
+	}
+}
