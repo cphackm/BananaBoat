d38754b3f6f911a3f1fbc211bb03fa24cb72582c
cphackm <cphackm@gmail.com>
2014-06-28 21:12:51 -0700

    Initial commit

---
+++ BananaBoat/GameObjects/AbstractGameObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace BananaBoat.GameObjects
+{
+	public abstract class AbstractGameObject
+	{
+		private int id { get; set; }
+		private float x { get; set; }
+		private float y { get; set; }
+
+		public abstract bool Update();
+		public abstract bool Render();
+	}
+}
